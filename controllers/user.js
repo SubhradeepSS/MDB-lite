bc30f9468673f15c6d154014cd9d36c455a573f3
Waryserpant122 <yashshah123boy@gmail.com>
2021-08-05 02:53:28 +0530

    feat:added cascade delete using pre hook for ratings from movie schema

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,20 @@
-const { User } = require('../models/user')
-const { Rating } = require('../models/rating')
+const { User } = require("../models/user");
+const { Rating } = require("../models/movie");
 
 const getAllUsers = (req, res) => {
-    User.find({}).then(users => {
-        res.render('mdb/user/viewUsers', { users })
-    })
-}
+  User.find({}).then((users) => {
+    res.render("mdb/user/viewUsers", { users });
+  });
+};
 
 const deleteUser = (req, res) => {
-    User.findOne({ username: req.params.username }).then(user => {
-        Rating.deleteMany({ user }).then(() => {
-            user.remove().then(() => {
-                res.redirect('/mdb/users/')
-            })
-        })
-    }) 
-}
+  User.findOne({ username: req.params.username }).then((user) => {
+    Rating.deleteMany({ user }).then(() => {
+      user.remove().then(() => {
+        res.redirect("/mdb/users/");
+      });
+    });
+  });
+};
 
-module.exports = { getAllUsers, deleteUser }
+module.exports = { getAllUsers, deleteUser };
--- routes/movie.js
+++ routes/movie.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { ensureAuthenticated } = require("../conf/auth.js")
+const { ensureAuthenticated } = require("../conf/auth.js");
 
-const { 
-    getAllMovies,
-    getMovie,
-    deleteMovie,
-    editMovie,
-    editMovieSubmit,
-    handleMovieAdd
-} = require('../controllers/movie')
+const {
+  getAllMovies,
+  getMovie,
+  deleteMovie,
+  editMovie,
+  editMovieSubmit,
+  handleMovieAdd,
+} = require("../controllers/movie");
 
-const { getRatings, addRating } = require('../controllers/rating')
+const { getRatings, addRating } = require("../controllers/rating");
 
-const { getBlog, addBlog } = require('../controllers/blog')
+const { getBlog, addBlog } = require("../controllers/blog");
 
-const { addComment } = require('../controllers/comment')
+const { addComment } = require("../controllers/comment");
 
-router.get('/addMovie', ensureAuthenticated, (req, res) => {
-    res.render('mdb/movie/addMovie')
-})
-router.post('/addMovie', ensureAuthenticated, handleMovieAdd);
+router.get("/addMovie", ensureAuthenticated, (req, res) => {
+  res.render("mdb/movie/addMovie");
+});
+router.post("/addMovie", ensureAuthenticated, handleMovieAdd);
 
-router.get('/', ensureAuthenticated, getAllMovies)
-router.get('/:name', ensureAuthenticated, getMovie)
-router.get('/delete/:name', ensureAuthenticated, deleteMovie)
+router.get("/", ensureAuthenticated, getAllMovies);
+router.get("/:name", ensureAuthenticated, getMovie);
+router.get("/delete/:name", ensureAuthenticated, deleteMovie);
 
-router.get('/edit/:name', ensureAuthenticated, editMovie)
-router.post('/edit/:name', ensureAuthenticated, editMovieSubmit)
+router.get("/edit/:name", ensureAuthenticated, editMovie);
+router.post("/edit/:name", ensureAuthenticated, editMovieSubmit);
 
-router.get('/:name/ratings', ensureAuthenticated, getRatings)
-router.post('/:name/ratings', ensureAuthenticated, addRating)
+router.get("/:name/ratings", ensureAuthenticated, getRatings);
+router.post("/:name/ratings", ensureAuthenticated, addRating);
 
-router.post('/:name/blog', ensureAuthenticated, getBlog)
-router.post('/:name/blog/add', ensureAuthenticated, addBlog)
+router.post("/:name/blog", ensureAuthenticated, getBlog);
+router.post("/:name/blog/add", ensureAuthenticated, addBlog);
 
-router.post('/comment', ensureAuthenticated, addComment)
+router.post("/comment", ensureAuthenticated, addComment);
 
-module.exports = router
+module.exports = router;
